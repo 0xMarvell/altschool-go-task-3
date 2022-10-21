69fad2d243cb25d6692acaf70ac56f8672bf5543
0xMarvell <rokemarvellous@gmail.com>
2022-10-21 16:10:39 +0100

    feat: add welcome message

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/0xMarvell/altschool-go-task-3/pkg/atm"
+)
 
 func main() {
 	log.Println("hello world")
+	atm.WelcomeScreen()
+	atm.Menu()
 }
--- pkg/atm/atm.go
+++ pkg/atm/atm.go
@@ -7,11 +7,18 @@ import (
 
 // WelcomeScreen displays welcome message.
 func WelcomeScreen() {
+	fmt.Println("------------------------")
+	fmt.Println("          ATM          ")
+	fmt.Println("------------------------")
 
 }
 
 // Menu displays all available operations to user.
 func Menu() {
+	fmt.Println("AVAILABLE OPERATIONS")
+	fmt.Println("1. Check Balance \t 2. Withdraw")
+	fmt.Println("3. Deposit \t\t 4. Change PIN")
+	fmt.Println("\t\t\t 0. Exit")
 
 }
 
